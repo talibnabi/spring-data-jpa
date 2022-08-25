6ed4dca4477a8e405dbef959fb58c680e0e8e5d7
talibnabi <talibnabiyev@gmail.com>
2022-08-25 06:18:22 -0700

    commit

--- spring-data-jpa-2/src/main/java/com/company/springdatajpa/entity/Guardian.java
+++ spring-data-jpa-2/src/main/java/com/company/springdatajpa/entity/Guardian.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 
@@ -11,8 +14,22 @@ import javax.persistence.Embeddable;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@AttributeOverrides({
+        @AttributeOverride(
+                name = "name",
+                column = @Column(name = "guardian_name")
+        ),
+        @AttributeOverride(
+                name = "email",
+                column = @Column(name = "guardian_email")
+        ),
+        @AttributeOverride(
+                name = "mobile",
+                column = @Column(name = "guardian_mobile_number")
+        )
+})
 public class Guardian {
     private String name;
     private String email;
-    private String mobileNumber;
+    private String mobile;
 }
