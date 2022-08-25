fe70bda2772fcc1547925c84869b945c58f22275
talibnabi <talibnabiyev@gmail.com>
2022-08-25 04:58:51 -0700

    commit

--- spring-data-jpa-2/src/test/java/com/company/springdatajpa/repository/StudentRepositoryTest.java
+++ spring-data-jpa-2/src/test/java/com/company/springdatajpa/repository/StudentRepositoryTest.java
@@ -1,12 +1,35 @@
 package com.company.springdatajpa.repository;
 
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import com.company.springdatajpa.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@DataJpaTest
 class StudentRepositoryTest {
-    
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    public void saveStudent() {
+        Student student = Student.builder()
+                .emailId("talibnabi@gmail.com")
+                .firstName("Talib")
+                .lastName("Nabi")
+                .guardianName("Alisa")
+                .guardianEmail("alisaasln@gmail.com")
+                .guardianMobile("0709533113")
+                .build();
+        studentRepository.save(student);
+    }
+
+    @Test
+    public void printAllStudent() {
+        List<Student> studentList = studentRepository.findAll();
+        studentList.forEach(System.out::println);
+    }
 }
