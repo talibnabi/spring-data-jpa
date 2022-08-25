f4bebab1134e7a32e3f5f5f61c7f23f6ddfc9590
talibnabi <talibnabiyev@gmail.com>
2022-08-25 04:24:33 -0700

    commit

--- spring-data-jpa-2/src/test/java/com/company/springdatajpa/SpringDataJpaApplicationTests.java
+++ spring-data-jpa-2/src/test/java/com/company/springdatajpa/SpringDataJpaApplicationTests.java
@@ -8,6 +8,7 @@ class SpringDataJpaApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
---
+++ spring-data-jpa-2/src/test/java/com/company/springdatajpa/repository/StudentRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.company.springdatajpa.repository;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@DataJpaTest
+class StudentRepositoryTest {
+    
+}
