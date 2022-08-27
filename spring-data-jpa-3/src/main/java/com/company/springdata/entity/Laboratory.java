81ab65c73b81aa937e92c592ccb598ce102cba4a
talibnabi <talibnabiyev@gmail.com>
2022-08-27 14:34:09 -0700

    commit

--- spring-data-jpa-3/src/main/java/com/company/springdata/entity/Laboratory.java
+++ spring-data-jpa-3/src/main/java/com/company/springdata/entity/Laboratory.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
@@ -14,6 +14,19 @@ import java.time.LocalDateTime;
 @Data
 @Builder
 public class Laboratory {
+    @SequenceGenerator(
+            name = "laboratory_sequence",
+            sequenceName = "laboratory_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "laboratory_sequence"
+    )
+    @Column(name = "laboratory_id")
     private Long laboratoryId;
+
+    @Column(name = "lab_date", nullable = false)
     private LocalDateTime labDate;
 }
