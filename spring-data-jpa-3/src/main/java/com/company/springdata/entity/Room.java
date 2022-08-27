74aaa4ee679d3719f72b49f0c62ea81195989fc6
talibnabi <talibnabiyev@gmail.com>
2022-08-27 14:39:46 -0700

    commit

--- spring-data-jpa-3/src/main/java/com/company/springdata/entity/Room.java
+++ spring-data-jpa-3/src/main/java/com/company/springdata/entity/Room.java
@@ -6,15 +6,31 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
+@Table(name = "tbl_room")
 public class Room {
+    @SequenceGenerator(
+            name = "out_patient_sequence",
+            sequenceName = "out_patient_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "out_patient_sequence"
+    )
+    @Column(name = "room_id")
     private Long roomId;
+
+    @Column(name = "room_type", nullable = false)
     private String roomType;
+
+    @Column(name = "status", nullable = false)
     private String status;
 }
