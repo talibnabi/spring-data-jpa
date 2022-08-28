5fcf6689c9c967d528ad7b46b0c3965bc5bdbf7b
talibnabi <talibnabiyev@gmail.com>
2022-08-28 08:03:38 -0700

    commit

--- spring-data-jpa-3/src/main/java/com/company/springdata/repository/BillRepository.java
+++ spring-data-jpa-3/src/main/java/com/company/springdata/repository/BillRepository.java
@@ -1,11 +1,45 @@
 package com.company.springdata.repository;
 
 import com.company.springdata.entity.Bill;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BillRepository extends JpaRepository<Bill, Long> {
     List<Bill> findByDayCount(Integer dayCount);
+    //TODO: JPQL
 
+    @Query("select b from Bill b where b.dayCount=?1 and b.doctorCharge=?2")
+    Bill getBillByDayCountAndDoctorCharge(Integer dayCount, Integer doctorCharge);
+
+    @Query("select b.dayCount from Bill b where b.doctorCharge=?1")
+    List<Integer> getDayCountByDoctorCharge(Integer doctorCharge);
+
+    @Query("select b.roomCharge from Bill b where b.doctorCharge>?1 and b.labCharge<?1 order by b.dayCount")
+    List<Integer> getRoomChargeByDoctorChargeAndLabCharge(Integer doctorCharge, Integer labCharge);
+
+    @Query("select b from Bill b where b.roomCharge<?1 and b.labCharge>?2 order by b.roomCharge")
+    Page<Bill> getBillByRoomChargeAndLabCharge(Integer roomCharge, Integer labCharge, Pageable pageable);
+
+
+    //TODO: NATIVE QUERY
+
+    @Query(
+            value = "select * from tbl_bill b where b.bill_id=?1 and b.room_charge=?2",
+            nativeQuery = true
+    )
+    Bill getBillByBillIdAndRoomCharge(Integer billId, Integer roomCharge);
+
+    @Query(
+            value = "select * from tbl_bill b where ",
+            nativeQuery = true
+    )
+    Bill getBillByBillIdAndLabCharge(
+            @Param("billId") Integer billId,
+            @Param("labCharge") Integer labCharge
+    );
 }
